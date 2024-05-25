93b9ea35c15450001963c74400b22c17e56c390b
eric-the-geric <eric.gericke2@gmail.com>
2024-05-25 14:43:41 +0200

    added dot env and added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+from dotenv import load_dotenv
+import os
+ROOT_DIR = '.'
+load_dotenv(ROOT_DIR + "/config.env")
+api_key = os.environ["MISTRAL_API_KEY"]
+model = "mistral-large-latest"
+client = MistralClient(api_key=api_key)
+
+messages = [
+    ChatMessage(role="user", content="What is the best French cheese?")
+]
+
+# No streaming
+chat_response = client.chat(
+    model=model,
+    messages=messages,
+)
+
+print(chat_response.choices[0].message.content)
